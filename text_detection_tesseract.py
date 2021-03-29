b60c0c392d6b503859c6ae9c5111a0e4c61f507e
maqerfoo <jakobpoulsen93@gmail.com>
2021-03-29 16:19:06 +0200

    added test images and tesseract text detection script

---
+++ text_detection_tesseract.py
@@ -0,0 +1,33 @@
+#needs Tesseract4 and pytesseract installed
+
+# import the necessary packages
+from pytesseract import Output
+import pytesseract
+import argparse
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+ap.add_argument("-c", "--min-conf", type=int, default=0,
+	help="mininum confidence value to filter weak text detection")
+args = vars(ap.parse_args())
+
+# load the input image, convert it from BGR to RGB channel ordering,
+# and use Tesseract to localize each area of text in the input image
+image = cv2.imread(args["image"])
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+results = pytesseract.image_to_data(rgb, output_type=Output.DICT)
+
+# loop over each of the individual text localizations
+for i in range(0, len(results["text"])):
+	# extract the bounding box coordinates of the text region from
+	# the current result
+	x = results["left"][i]
+	y = results["top"][i]
+	w = results["width"][i]
+	h = results["height"][i]
+	# extract the OCR text itself along with the confidence of the
+	# text localization
+	text = results["text"][i]
+	conf = int(results["conf"][i])
