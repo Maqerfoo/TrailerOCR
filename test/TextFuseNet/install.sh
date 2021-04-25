247d5c24d593b2a09d1a94120af0cb054f88c7f5
maqerfoo <jakobpoulsen93@gmail.com>
2021-04-25 22:08:28 +0200

    simplified install script. Activate after conda environment is created and activated

--- test/TextFuseNet/install.sh
+++ test/TextFuseNet/install.sh
@@ -1,23 +1,15 @@
-ENVS=$(conda env list | awk '{print $1}' )
-if [[ $ENVS = *"textfusenet"* ]]; then
-   conda activate textfusenet
-else 
-   conda create --name textfusenet python=3.7.3
-   conda activate textfusenet
-   conda install pytorch=1.3.1 torchvision cudatoolkit=10.1 -c pytorch
-   pip install opencv-python
-   pip install tensorboard
-   pip install yacs
-   pip install tqdm
-   pip install termcolor
-   pip install tabulate
-   pip install matplotlib
-   pip install cloudpickle
-   pip install wheel
-   pip install pycocotools
-   pip install gdown
-   exit
-fi
+conda install pytorch=1.3.1 torchvision cudatoolkit=10.1 -c pytorch
+pip install opencv-python
+pip install tensorboard
+pip install yacs
+pip install tqdm
+pip install termcolor
+pip install tabulate
+pip install matplotlib
+pip install cloudpickle
+pip install wheel
+pip install pycocotools
+pip install gdown
 pip install fvcore-master.zip
 python setup.py build develop
 cd out_dir_r101
