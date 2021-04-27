f6b8d17f49b5c62f63b313c53ee371c957ba2924
maqerfoo <jakobpoulsen93@gmail.com>
2021-04-27 12:18:11 +0200

    cleaned repo

--- main.sh
+++
@@ -1,9 +0,0 @@
-for file in /images/*
-do
-    # 1)find text
-    # 2)extract coordinates
-    python image_correction.py --image $file --coords "[(63, 242), (291, 110), (361, 252), (78, 386)]"
-    # 3)correct the skew ^
-    # 4)greyscale + binarization
-    # 5)OCR engine, read text
-done
