b21738a201451e100d3f8279a5fd6ee6c4817853
maqerfoo <jakobpoulsen93@gmail.com>
2021-05-04 12:08:34 +0200

    remove res_ from filenames

--- TextFuseNet/demo/icdar2015_detection.py
+++ TextFuseNet/demo/icdar2015_detection.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
 
         prediction, vis_output, polygons = detection_demo.run_on_image(img)
 
-        txt_save_path = output_path + 'res_' + img_name.split('.')[0] + '.txt'
+        txt_save_path = output_path + img_name.split('.')[0] + '.txt'
         save_result_to_txt(txt_save_path,prediction,polygons)
 
         print("Time: {:.2f} s / img".format(time.time() - start_time))
